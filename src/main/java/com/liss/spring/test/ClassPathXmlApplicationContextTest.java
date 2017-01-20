fe1f65410c04da901875b792160722d3db2b22ca
liss <liss_shun@outlook.com>
2017-01-20 15:29:52 +0800

    exclude target

---
+++ src/main/java/com/liss/spring/test/ClassPathXmlApplicationContextTest.java
@@ -0,0 +1,17 @@
+package com.liss.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.liss.spring.dao.IUserDAO;
+
+public class ClassPathXmlApplicationContextTest {
+	public static void main(String[] args) {
+		ApplicationContext context =
+			    new ClassPathXmlApplicationContext("applicationContext.xml");
+		System.out.println(context.getBean("user"));
+		IUserDAO dao = (IUserDAO) context.getBean("IUserImpl");
+		dao.getUserById("1");
+		System.out.println(context.getBean("dataSource"));
+	}
+}
--- src/main/java/com/liss/spring/test/FileSystemXmlApplicationContextTest.java
+++ src/main/java/com/liss/spring/test/FileSystemXmlApplicationContextTest.java
@@ -2,13 +2,14 @@ package com.liss.spring.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.liss.spring.dao.IUserDAO;
 
-public class ApplicationContextUtils {
+public class FileSystemXmlApplicationContextTest {
 	public static void main(String[] args) {
 		ApplicationContext context =
-			    new ClassPathXmlApplicationContext("applicationContext.xml");
+			    new FileSystemXmlApplicationContext("D:\\workspace\\eclipse-jee-mars-2-win32-x86_64\\SpringMVC\\src\\main\\resources\\applicationContext.xml");
 		System.out.println(context.getBean("user"));
 		IUserDAO dao = (IUserDAO) context.getBean("IUserImpl");
 		dao.getUserById("1");
